ae3edcd98be355348b105bc2da5bd86605c86c1c
Will Crawford <william.e.crawford@accenture.com>
2024-06-01 17:04:45 -0700

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApplianceCardComponent } from './components/appliance-card/appliance-card.component';
 import { BuilderComponent } from './pages/builder/builder.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, BuilderComponent, ApplianceCardComponent]
+  imports: [RouterOutlet, BuilderComponent, ApplianceCardComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'ogpp-v2';
